93afd592f2b86681d7d80bbc2ca3255e7a3e4857
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-19 16:48:27 -0600

    square card info styled

--- components/myComponents/componentInfo/SquareCardInfo.tsx
+++ components/myComponents/componentInfo/SquareCardInfo.tsx
@@ -1,24 +1,43 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import CenterView from '../CenterView'
 import SimpleSquareCard from '../SimpleSquareCard'
 
 const SquareCardInfo = () => {
     return(
         <CenterView>
-        <SimpleSquareCard color='#8a5b53' textColor='white' onPress={() => {}}>
+        <SimpleSquareCard color='blue' textColor='white' onPress={() => {}}>
             Square Card
         </SimpleSquareCard>
-        <Text>Square card</Text>
-        <View>
-        <Text>Required Props:</Text>
+        <View style={styles.textContainer}>
+        <Text style={styles.text}>Required Props:</Text>
         <Text>Color: Sets card color</Text>
         <Text>textColor: sets text color</Text>
         <Text>onPress: determines what card will do onPress</Text>
+        </View>
+        <Text style={styles.text}>props.children should be a text component, by default it is aligned and justified at flex end.</Text>
+
+        <View style={styles.textContainer}>
+        <Text style={styles.text}>
+            Optional Props:
+            </Text>
+            <Text>style: overwrites default styles</Text>
         </View>
         </CenterView>
 
     )
 }
+
+const styles = StyleSheet.create({
+    textContainer: {
+        alignItems: 'center',
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 15,
+        marginVertical: 10,
+        paddingHorizontal: 15,
+    }
+})
 
 export default SquareCardInfo
