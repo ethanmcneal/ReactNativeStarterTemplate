df31980c5bfdd2d6c9924ffd060556c69307e792
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-19 13:18:34 -0600

    color scheme being used in navigation

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -7,26 +7,42 @@ import SquareCardInfo from "../components/myComponents/componentInfo/SquareCardI
 import CenterViewInfo from "../components/myComponents/componentInfo/CenterViewInfo";
 import { Ionicons } from "@expo/vector-icons";
 import React from 'react'
+import { useThemeColor } from "../components/Themed";
+import { Appearance } from "react-native";
+import Colors from "../constants/Colors";
 
 
 
-
+const colorScheme = Appearance.getColorScheme()
+const navigationOptions = {
+    headerStyle: {
+        backgroundColor: colorScheme === 'dark' ? Colors.dark.background : Colors.light.background,
+        
+    }, 
+    headerTintColor: colorScheme === 'dark' ? Colors.dark.text : Colors.light.text
+}
 const ComponentNavigator = createStackNavigator({
     MyComponents: MyComponents,
     SquareCard: SquareCardInfo,
     CenterView: CenterViewInfo
+},  {
+    defaultNavigationOptions: navigationOptions
 })
 
 const HomeNavigator = createStackNavigator({
     Home: Home,
+}, {
+    defaultNavigationOptions: navigationOptions
 })
 
 const MainNavigator = createBottomTabNavigator({
     Home: HomeNavigator,
     Components: ComponentNavigator
 },
+
 {
     defaultNavigationOptions: ({ navigation }) => ({
+      
       tabBarIcon: ({ focused, horizontal, tintColor }) => {
         const { routeName } = navigation.state;
         let IconComponent = Ionicons;
@@ -44,9 +60,15 @@ const MainNavigator = createBottomTabNavigator({
       },
     }),
     tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
+      activeTintColor: colorScheme == 'dark' ? Colors.dark.tabIconSelected : Colors.light.tabIconSelected,
+      inactiveTintColor: colorScheme == 'dark' ? Colors.dark.tabIconDefault : Colors.light.tabIconDefault,
+      activeBackgroundColor: colorScheme == 'dark' ? Colors.dark.background : Colors.light.background,
+      style: {
+        backgroundColor: colorScheme === 'dark' ? Colors.dark.background : Colors.light.background,
     },
+    },
+    
+     
   })
 
 export default createAppContainer(MainNavigator)
